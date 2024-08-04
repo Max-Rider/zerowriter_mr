77af21dbe1dd7987435c240412c8a81c1493f509
Max Rider <ridermax@comcast.net>
2024-08-04 18:54:22 -0400

    messing with rotate

--- e-Paper/RaspberryPi_JetsonNano/python/examples/zerowriter.py
+++ e-Paper/RaspberryPi_JetsonNano/python/examples/zerowriter.py
@@ -159,7 +159,7 @@ class ZeroWriter:
         
         self.display = AutoEPDDisplay(vcom=-1.50)
         self.display.clear()
-        self.display._set_rotate(None, True)
+        self.display._set_rotate('flip', True)
         
         self.display.epd.wait_display_ready()
         
